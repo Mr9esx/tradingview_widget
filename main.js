28afc3de6221a212f82d2ee75ac41333a6e80fde
Mr9esx <1138099359@qq.com>
2018-11-06 14:02:06 +0800

    version 0.1.4

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 // Modules to control application life and create native browser window
+const electron = require('electron')
 const {app, BrowserWindow} = require('electron')
 const {ipcMain} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let yLock = false
+let xLock = false
+let showing = false
 
 function createWindow () {
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 395, height: 350, transparent: true, frame: false, minimizable: false, maximizable: false, fullscreen: false, resizable: false})
+  mainWindow = new BrowserWindow({width: 376, height: 293, minWidth: 376, minHeight: 293, transparent: true, frame: false, minimizable: false, maximizable: false, fullscreen: false, resizable: false})
 
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // mainWindow.webContents.openDevTools()
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -27,7 +30,48 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  // mainWindow.on('move', function() {
+  //   let position = mainWindow.getPosition()
+  //
+  //   if (position[1] >= 10 && yLock) {
+  //     yLock = false
+  //   }
+  //   if (position[1] < 10 && !yLock) {
+  //     mainWindow.setPosition(position[0], 0)
+  //     mainWindow.setSize(376, 293)
+  //     yLock = true
+  //   }
+  // });
+
+  // mainWindow.on('blur', hideWindow);
+  //
+  // mainWindow.on('focus', showWindow);
+
+  mainWindow.on('resize', function() {
+    if (yLock) {
+      mainWindow.setSize(376, 293)
+    }
+  });
 }
+
+// function showWindow() {
+//   let position = mainWindow.getPosition()
+//   if (position[1] < 0 && yLock) {
+//     for (let i = position[1]; i <= 0; i++) {
+//       mainWindow.setPosition(position[0], i)
+//     }
+//   }
+// }
+//
+// function hideWindow() {
+//   let position = mainWindow.getPosition()
+//   if (position[1] === 0 && yLock) {
+//     for (let i = 0; i > -273; i--) {
+//       mainWindow.setPosition(position[0], i)
+//     }
+//   }
+// }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -51,9 +95,29 @@ app.on('activate', function () {
   }
 })
 
-ipcMain.on('closeWindow', (event, arg) => {
-  mainWindow.close()
-});
+ipcMain.on('close-window', (event, arg) => {
+  mainWindow.close();
+})
+
+ipcMain.on('pin-window', (event, arg) => {
+  if (mainWindow.isAlwaysOnTop()) {
+    mainWindow.setAlwaysOnTop(false)
+  } else {
+    mainWindow.setAlwaysOnTop(true)
+  }
+})
+
+// ipcMain.on('show-window', (event, arg) => {
+//   if (yLock) {
+//     showWindow()
+//   }
+// })
+//
+// ipcMain.on('hide-window', (event, arg) => {
+//   if (yLock) {
+//     hideWindow()
+//   }
+// })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,37 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// const {BrowserWindow} = require('electron').remote
+
+const {ipcRenderer} = require('electron')
+
+const bodyContent = document.getElementById('body-content')
+
+const closeWindow = document.getElementById('close-window')
+
+const pinWindow = document.getElementById('pin-window')
+
+let flag = false
+
+
+closeWindow.addEventListener('click', () => {
+  ipcRenderer.send('close-window')
+})
+
+pinWindow.addEventListener('click', () => {
+  ipcRenderer.send('pin-window')
+})
+
+// bodyContent.addEventListener('mouseover', () => {
+//   flag = false
+//   ipcRenderer.send('show-window')
+// })
+//
+// bodyContent.addEventListener('mouseout', () => {
+//   setTimeout(function () {
+//     if (!flag) {
+//       ipcRenderer.send('hide-window')
+//       flag = true
+//     }
+//   }, 300)
+// })

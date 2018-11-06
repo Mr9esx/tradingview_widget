0b677102aa05f98431fa0598dbcfa06629e4a7d8
Mr9esx <1138099359@qq.com>
2018-11-06 17:53:43 +0800

    feat: 软件显示在托盘中，不占用任务栏；添加图表；添加刷新按钮；置顶按钮添加样式直观显示是否置顶；添加blur时的样式，更直观区分是否focus；fix：更换图标。

--- main.js
+++ main.js
@@ -1,123 +1,80 @@
-// Modules to control application life and create native browser window
 const electron = require('electron')
 const {app, BrowserWindow} = require('electron')
 const {ipcMain} = require('electron')
+const {Tray} = require('electron');
+const {Menu} = require('electron');
+const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-let yLock = false
-let xLock = false
-let showing = false
+let contextMenu
+let appTray = null
+let trayMenuTemplate = null
 
-function createWindow () {
-  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 376, height: 293, minWidth: 376, minHeight: 293, transparent: true, frame: false, minimizable: false, maximizable: false, fullscreen: false, resizable: false})
+function createWindow() {
+    const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
 
-  // mainWindow.webContents.openDevTools()
+    mainWindow = new BrowserWindow({
+        width: 376,
+        height: 293,
+        minWidth: 376,
+        minHeight: 293,
+        transparent: true,
+        frame: false,
+        minimizable: false,
+        maximizable: false,
+        fullscreen: false,
+        resizable: false,
+        show: false,
+        skipTaskbar: true
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    trayMenuTemplate = [
+        {
+            label: '关闭',
+            click: function () {
+                quitApp()
+            }
+        }
+    ];
 
-  // mainWindow.on('move', function() {
-  //   let position = mainWindow.getPosition()
-  //
-  //   if (position[1] >= 10 && yLock) {
-  //     yLock = false
-  //   }
-  //   if (position[1] < 10 && !yLock) {
-  //     mainWindow.setPosition(position[0], 0)
-  //     mainWindow.setSize(376, 293)
-  //     yLock = true
-  //   }
-  // });
+    appTray = new Tray(path.join(path.join(__dirname, 'res'), 'logo.png'));
 
-  // mainWindow.on('blur', hideWindow);
-  //
-  // mainWindow.on('focus', showWindow);
+    contextMenu = Menu.buildFromTemplate(trayMenuTemplate);
 
-  mainWindow.on('resize', function() {
-    if (yLock) {
-      mainWindow.setSize(376, 293)
-    }
-  });
+    appTray.setToolTip('TradingView Widget');
+
+    appTray.setContextMenu(contextMenu);
+
+    appTray.on('double-click', (event, arg) => {
+        mainWindow.setAlwaysOnTop(true)
+        mainWindow.setAlwaysOnTop(false)
+    })
 }
 
-// function showWindow() {
-//   let position = mainWindow.getPosition()
-//   if (position[1] < 0 && yLock) {
-//     for (let i = position[1]; i <= 0; i++) {
-//       mainWindow.setPosition(position[0], i)
-//     }
-//   }
-// }
-//
-// function hideWindow() {
-//   let position = mainWindow.getPosition()
-//   if (position[1] === 0 && yLock) {
-//     for (let i = 0; i > -273; i--) {
-//       mainWindow.setPosition(position[0], i)
-//     }
-//   }
-// }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+function quitApp() {
+    appTray.destroy()
+    app.quit()
+}
+
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        quitApp()
+    }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 ipcMain.on('close-window', (event, arg) => {
-  mainWindow.close();
+    mainWindow.close();
 })
-
-ipcMain.on('pin-window', (event, arg) => {
-  if (mainWindow.isAlwaysOnTop()) {
-    mainWindow.setAlwaysOnTop(false)
-  } else {
-    mainWindow.setAlwaysOnTop(true)
-  }
-})
-
-// ipcMain.on('show-window', (event, arg) => {
-//   if (yLock) {
-//     showWindow()
-//   }
-// })
-//
-// ipcMain.on('hide-window', (event, arg) => {
-//   if (yLock) {
-//     hideWindow()
-//   }
-// })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -4,34 +4,126 @@
 // const {BrowserWindow} = require('electron').remote
 
 const {ipcRenderer} = require('electron')
+const closeWindowBtn = document.getElementById('close-window')
+const pinWindowBtn = document.getElementById('pin-window')
+const reloadWindowBtn = document.getElementById('reload-window')
+const windowTitle = document.getElementsByClassName('tradingview-widget-copyright')[0]
+const {remote} = require('electron')
+const app = remote.app
+require('./res/tv.js')
 
-const bodyContent = document.getElementById('body-content')
-
-const closeWindow = document.getElementById('close-window')
-
-const pinWindow = document.getElementById('pin-window')
-
+let mainWindow = remote.getCurrentWindow()
+let yLock = false
 let flag = false
 
-
-closeWindow.addEventListener('click', () => {
-  ipcRenderer.send('close-window')
+mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    loadChart()
 })
 
-pinWindow.addEventListener('click', () => {
-  ipcRenderer.send('pin-window')
+mainWindow.on('closed', function () {
+    mainWindow = null
 })
 
-// bodyContent.addEventListener('mouseover', () => {
-//   flag = false
-//   ipcRenderer.send('show-window')
-// })
-//
-// bodyContent.addEventListener('mouseout', () => {
-//   setTimeout(function () {
-//     if (!flag) {
-//       ipcRenderer.send('hide-window')
-//       flag = true
+// mainWindow.on('move', function () {
+//     let position = mainWindow.getPosition()
+//     if (position[1] > 0) {
+//         yLock = false
 //     }
-//   }, 300)
+//     if (position[1] === 0) {
+//         yLock = true
+//     }
+// })
+
+mainWindow.on('blur', () => {
+    windowTitle.style.background = "#262626";
+})
+
+mainWindow.on('focus', () => {
+    windowTitle.style.background = "#131722";
+})
+
+// mainWindow.on('resize', function () {
+//     if (yLock) {
+//         mainWindow.setSize(376, 293)
+//     }
+// })
+
+closeWindowBtn.addEventListener('click', () => {
+    ipcRenderer.send('close-window')
+})
+
+pinWindowBtn.addEventListener('click', () => {
+    if (pinWindow()) {
+        pinWindowBtn.setAttribute("class", "utils_btn utils_btn_select")
+    } else {
+        pinWindowBtn.setAttribute("class", "utils_btn")
+    }
+})
+
+reloadWindowBtn.addEventListener('click', () => {
+    loadChart()
+})
+
+function pinWindow() {
+    if (mainWindow.isAlwaysOnTop()) {
+        mainWindow.setAlwaysOnTop(false)
+    } else {
+        mainWindow.setAlwaysOnTop(true)
+    }
+    return mainWindow.isAlwaysOnTop()
+}
+
+function loadChart() {
+    new TradingView.widget({
+        "width": 380,
+        "height": 290,
+        "symbol": "XAUUSD",
+        "interval": "15",
+        "timezone": "Etc/UTC",
+        "theme": "Dark",
+        "style": "1",
+        "locale": app.getLocale().replace('-', '_'),
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": true,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "studies": [
+            "MACD@tv-basicstudies"
+        ],
+        "container_id": "tradingview_44e89"
+    })
+}
+
+// function showWindow() {
+//     let position = mainWindow.getPosition()
+//     if (position[1] < 0 && flag) {
+//         contentWindow.removeEventListener('mouseleave', hideWindow(), false)
+//         for (let i = position[1]; i <= 0; i = i + 2) {
+//             mainWindow.setBounds({
+//                 width: 376, height: 293, x: position[0], y: i
+//             })
+//         }
+//         flag = false
+//         setTimeout(function () {
+//             contentWindow.addEventListener('mouseleave', hideWindow())
+//         }, 500)
+//     }
+//
+// }
+//
+// function hideWindow() {
+//     let position = mainWindow.getPosition()
+//     if (position[1] === 0 && !flag) {
+//         for (let i = 0; i > -273; i = i - 2) {
+//             mainWindow.setBounds({
+//                 width: 376, height: 293, x: position[0], y: i
+//             })
+//         }
+//         flag = true
+//     }
+// }
+//
+// contentWindow.addEventListener('mouseover', () => {
+//     showWindow()
 // })
